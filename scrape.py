3a80167d6e9449d44116c294dca96777dcec5123
Corey He <corey72he@gmail.com>
2025-03-21 12:01:00 -0400

    Migrated over from pypub to ebooklib, needs to implement cover image

--- scrape.py
+++ scrape.py
@@ -4,14 +4,15 @@ import re
 from pathlib import Path
 from pymongo import MongoClient
 import os, errno
-import pypub
+#import pypub
 import datetime
 from novel_template import NovelTemplate
 import logging
 import time
 import io
 import jsonschema
-
+from ebooklib import epub 
+from PIL import Image
 
 
 #MIGRATE OVER FROM PYPUB TO EBOOKLIB
@@ -76,6 +77,27 @@ def check_existing_book(bookID):
     else:
         return True
 
+
+def get_cover(title,novelURL,saveDirectory):
+    soup = bs4.BeautifulSoup(requests.get(novelURL).text, 'html.parser')
+    img_url = soup.find("div",{"class":"cover-art-container"}).find("img")
+    response =requests.get(img_url["src"],stream=True)
+    
+    if (saveDirectory.endswith("/")):
+        fileNameDir=f"{saveDirectory}{title}.jpg"
+    else:
+        fileNameDir=f"{saveDirectory}/{title}.jpg"
+    
+    if not response.ok:
+        pass
+    else:
+        if not (check_directory_exists(saveDirectory)):
+            make_directory(saveDirectory)
+        if not (check_directory_exists(fileNameDir)):
+            response=response.content
+            with open (fileNameDir,'wb') as f:
+                f.write(response)
+            
 
 def fetchNovelData(novelURL):
     soup = bs4.BeautifulSoup(requests.get(novelURL).text, 'html.parser')
@@ -207,8 +229,8 @@ def extract_chapter_ID(chapterURL):
     return chapter[len(chapter)-2]
 
 def extract_chapter_title(string):
-    extractedTitle=string.split("/")
-    extractedTitle=re.sub('.html','',extractedTitle)
+    extractedTitle=re.sub('.html','',string)
+    extractedTitle=extractedTitle.split("/")
     return extractedTitle[len(extractedTitle)-1]
     
     
@@ -223,7 +245,6 @@ def get_existing_order_of_contents(bookTitle):
         
 
 def write_order_of_contents(bookTitle, chapterData):
-    logging.warning(bookTitle)
     bookDirLocation=f"./books/raw/{bookTitle}"
     if not (check_directory_exists(bookDirLocation)):
         make_directory(bookDirLocation)
@@ -266,85 +287,115 @@ def check_if_chapter_exists(chapterID,savedChapters):
     return False
 
 def get_chapter_from_saved(chapterID,savedChapters):
+    
+    
+    #logging.warning(savedChapters)
+    
+    
     for chapter in savedChapters:
+        #logging.warning(chapter)
         chapter=chapter.split(";")
-        #logging.warning(chapterID+" "+chapter[0])
         if chapterID == chapter[0]:
             return chapter[0],chapter[2].replace("\n","")
+        
+        
+        
     return -1,-1
     
 def get_chapter_contents_from_saved(dirLocation):
-    logging.warning(dirLocation)
     f=open(dirLocation,"r")
     
     return f.read()
 
-def generate_Epub_Based_On_Stored_Order(bookTitle,author):
-    new_epub=pypub.Epub(bookTitle, creator=author)
+def generate_Epub_Based_On_Stored_Order(new_epub, bookTitle):
     already_saved_chapters=get_existing_order_of_contents(bookTitle)
-    chapterMetaData=list()
+    
+    tocList=list()
     for url in already_saved_chapters:
-        #logging.warning(url)
         url=url.split(";")
         chapterID=url[0]
-        chapterTitle=extract_chapter_title(url[len(url)-1])
+        fileChapterTitle=extract_chapter_title(url[len(url)-1])
         dirLocation=url[len(url)-1]
         chapterContent=get_chapter_contents_from_saved(dirLocation).encode("utf-8")
-            
         
+        strippedTitle=fileChapterTitle.split('-')
+        strippedTitle=strippedTitle[len(strippedTitle)-1].strip()
+        
+        chapter=epub.EpubHtml(title=strippedTitle,file_name=fileChapterTitle+'.xhtml',lang='en')
+        chapter.set_content(chapterContent)
+        
+        
+        tocList.append(chapter)
+        
+        new_epub.add_item(chapter)
     
-        new_chapter=pypub.create_chapter_from_html(chapterContent, chapterTitle)
-        new_epub.add_chapter(new_chapter)
-        time.sleep(0.5)
-    
+    new_epub.toc=tocList
     storeEpub(bookTitle,new_epub)
     
         
-def generate_Epub_Based_On_Online_Order(novelURL,bookTitle,author):
-    new_epub=pypub.Epub(bookTitle, creator=author)
+def generate_Epub_Based_On_Online_Order(new_epub,novelURL,bookTitle):
     
+    
+    tocList=list()
     for url in fetchChapterList(novelURL):
         chapterID=extract_chapter_ID(url)
         chapterTitle=fetchChapterTitle(url)
-        title = f"{bookTitle} - {chapterID} - {remove_invalid_characters(chapterTitle)}"
+        fileChapterTitle = f"{bookTitle} - {chapterID} - {remove_invalid_characters(chapterTitle)}"
         chapterContent=fetchChapter(url)
-        new_chapter=pypub.create_chapter_from_html(chapterContent, chapterTitle)
-        new_epub.add_chapter(new_chapter)
-        time.sleep(0.5)
         
+        
+        chapter=epub.EpubHtml(title=chapterTitle,file_name=fileChapterTitle+'.xhtml',lang='en')
+        chapter.set_content(chapterContent)
+        
+        tocList.append(chapter)
+        
+        new_epub.add_item(chapter)
+        
+        time.sleep(0.5)
+    new_epub.toc=tocList
     storeEpub(bookTitle,new_epub)
     
 
-def produceEpub(novelURL,bookTitle,Author):
-    new_epub=pypub.Epub(bookTitle, creator=Author)
+def produceEpub(new_epub,novelURL,bookTitle):
+    
     already_saved_chapters=get_existing_order_of_contents(bookTitle)
     chapterMetaData=list()
+    
+    tocList=list()
+    
     for url in fetchChapterList(novelURL):
-        #logging.warning(url)
         chapterID=extract_chapter_ID(url)
         chapterTitle=fetchChapterTitle(url)
         if (already_saved_chapters is False):
-            title = f"{bookTitle} - {chapterID} - {remove_invalid_characters(chapterTitle)}"
-            chapterMetaData.append([chapterID,url,f"./books/raw/{bookTitle}/{title}.html"])
+            fileChapterTitle = f"{bookTitle} - {chapterID} - {remove_invalid_characters(chapterTitle)}"
+            chapterMetaData.append([chapterID,url,f"./books/raw/{bookTitle}/{fileChapterTitle}.html"])
             chapterContent=fetchChapter(url)
+
             
             store_chapter(chapterContent,bookTitle,chapterTitle,chapterID)
             
         if (check_if_chapter_exists(chapterID,already_saved_chapters)):
-            logging.warning('Chapter exists')
             chapterID,dirLocation=get_chapter_from_saved(chapterID,already_saved_chapters)
             chapterContent=get_chapter_contents_from_saved(dirLocation).encode("utf-8")
-            chapterTitle=extract_chapter_title(dirLocation)
+            fileChapterTitle=extract_chapter_title(dirLocation)
+        
+        chapter=epub.EpubHtml(title=chapterTitle,file_name=fileChapterTitle+'.xhtml',lang='en')
+        chapter.set_content(chapterContent)
             
-        
-    
-        new_chapter=pypub.create_chapter_from_html(chapterContent, chapterTitle)
-        new_epub.add_chapter(new_chapter)
+        tocList.append(chapter)
+            
+        new_epub.add_item(chapter)
         time.sleep(0.5)
-        
+    
+    new_epub.toc=tocList
+    new_epub.spine=tocList
+    new_epub.add_item(epub.EpubNcx())
+    new_epub.add_item(epub.EpubNav())
+    #NCX and Navigation Tile is needed.
+    #DEFINITELY NEEDS A Spine.
+    
     if (already_saved_chapters is False):
         write_order_of_contents(bookTitle, chapterMetaData)
-    
     
     storeEpub(bookTitle,new_epub)
     
@@ -356,7 +407,7 @@ def storeEpub(bookTitle,new_epub):
     dirLocation="./epubs/"+bookTitle+"/"+bookTitle+".epub"
     if (check_directory_exists(dirLocation)):
         os.remove(dirLocation)
-    new_epub.create(dirLocation)
+    epub.write_epub(dirLocation,new_epub)
 
 def store_chapter(content,bookTitle, chapterTitle,chapterID):
     #remove invalid characters from file name
@@ -370,11 +421,9 @@ def store_chapter(content,bookTitle, chapterTitle,chapterID):
 
     #check if the chapter already exists
     title = f"{bookTitle} - {chapterID} - {chapterTitle}"
-    logging.warning(title)
     dirLocation=f"./books/raw/{bookTitle}/{title}.html"
     #if it is, don't store
     if check_directory_exists(dirLocation):
-        logging.warning("Chapter already stored")
         return
     #otherwise, do store the chapter.
     chapterDirLocation = "./books/raw/"+bookTitle+"/"
@@ -433,11 +482,15 @@ def mainInterface(novelURL):
     bookurl=bookurl.group()
     bookID,bookTitle,bookAuthor,description,lastScraped,latestChapter=fetchNovelData(bookurl)
     
-    
     if(check_latest_chapter(bookID,bookTitle,latestChapter)):
         directory=getEpub(bookID)
     else:
-        produceEpub(novelURL,bookTitle,bookAuthor)
+        new_epub=epub.EpubBook()
+        new_epub.set_identifier(bookID)
+        new_epub.set_title(bookTitle)
+        new_epub.set_language('en')
+        new_epub.add_author(bookAuthor)
+        produceEpub(new_epub,bookurl,bookTitle)
 
         
         
@@ -474,7 +527,6 @@ def mainInterface(novelURL):
 
 #Requires 9 inputs. BookID, bookName, bookAuthor, bookDescription, WebsiteHost, firstChapter#, lastChapter#, totalChapters, directory
 def create_Entry(**kwargs):
-    logging.warning(kwargs)
     default_values = {
         "bookID": 0,
         "bookName": "Template",
@@ -489,7 +541,6 @@ def create_Entry(**kwargs):
     #If missing keyword arguments, fill with template values.
     book_data = {**default_values, **kwargs}
     
-    logging.warning(book_data)
     book = {
         "bookID": book_data["bookID"],
         "bookName": book_data["bookName"],
@@ -516,7 +567,7 @@ def get_Entry(bookID):
 
 #logging.warning(read_Entry(54046))
 #mainInterface("https://www.royalroad.com/my/follows")
-#mainInterface("https://www.royalroad.com/fiction/54046/final-core-a-holy-dungeon-core-litrpg")
+mainInterface("https://www.royalroad.com/fiction/54046/final-core-a-holy-dungeon-core-litrpg")
 
 
 @app.get("/")
